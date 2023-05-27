f86f7248772f4b7475571223a488884c3d61660f
Rukin Danil <rukin2018d@gmail.com>
2023-05-27 15:18:42 +0700

    position configuration

---
+++ src/AlphaTechnologies.ReportCard.Data/Configurations/PositionEntityTypeConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using AlphaTechnologies.ReportCard.Domain.EmployeeAgregate;
+using AlphaTechnologies.ReportCard.Domain.PositionEntity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphaTechnologies.ReportCard.Data.Configurations
+{
+    public class PositionEntityTypeConfiguration : IEntityTypeConfiguration<Position>
+    {
+        public void Configure(EntityTypeBuilder<Position> builder)
+        {
+            builder.ToTable(DataConstants.POSITIONS_TABLE_NAME);
+
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Id).ValueGeneratedOnAdd();
+
+            builder.Ignore(p => p.DomainEvents);
+            builder.Ignore(p => p.Emloyees);
+
+            builder.HasMany<Employee>(DataConstants.POSITIONS_EMPLOYEES)
+                .WithMany(DataConstants.EMPLOYEES_POSITIONS)
+                .UsingEntity(
+                    DataConstants.EMPLOYEE_POSITION_TABLE_NAME,
+                    left => left.HasOne(typeof(Employee)).WithMany()
+                    .HasForeignKey("id_employee").OnDelete(DeleteBehavior.Cascade),
+                    right => right.HasOne(typeof(Position)).WithMany()
+                    .HasForeignKey("id_position").OnDelete(DeleteBehavior.Cascade)
+                );
+            builder.Navigation(DataConstants.POSITIONS_EMPLOYEES)
+                .UsePropertyAccessMode(PropertyAccessMode.Field);
+        }
+    }
+}
