39a54a4cfbcca63c7cc6fa63dfd216bd52f0079a
Rukin Danil <rukin2018d@gmail.com>
2023-05-27 19:38:03 +0700

    first integration test for data layer with sqlite in memory

---
+++ tests/AlphaTechnologies.ReportCard.IntegrationTests/Data/DataAccessTests.cs
@@ -0,0 +1,28 @@
+﻿using AlphaTechnologies.ReportCard.Domain.DepartmentAgregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphaTechnologies.ReportCard.IntegrationTests.Data
+{
+    public class DataAccessTests : EfCoreBaseFixture
+    {
+        [Fact]
+        public void AddDepartments_DepartmentShouldBeAdded()
+        {
+            var context = GetClearContext();
+            context.Database.EnsureCreated();
+            Department department = new Department("name");
+            Assert.True(department.Id == 0);
+
+            context.Add(department);
+            context.SaveChanges();
+
+            Department result = context.Departments.First();
+            Assert.NotNull(result);
+            Assert.True(result.Id > 0);
+        }
+    }
+}
---
+++ tests/AlphaTechnologies.ReportCard.IntegrationTests/Data/EfCoreBaseFixture.cs
@@ -0,0 +1,38 @@
+﻿using AlphaTechnologies.ReportCard.Data;
+using AlphaTechnologies.ReportCard.SharedKernel.Interfaces;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphaTechnologies.ReportCard.IntegrationTests.Data
+{
+    public class EfCoreBaseFixture
+    {
+        protected SqliteConnection _connection;
+        protected AlphaTechnologiesRepordCardDbContext GetClearContext()
+        {
+            var options = CreateDbContextOptions();
+            var mockEventDispatcher = new Mock<IDomainEventDispatcher>();
+
+            return new AlphaTechnologiesRepordCardDbContext(mockEventDispatcher.Object, options);
+        }
+
+        protected DbContextOptions<AlphaTechnologiesRepordCardDbContext> CreateDbContextOptions()
+        {
+            _connection = new SqliteConnection("Filename=:memory:");
+            _connection.Open();
+            
+
+            var builder = new DbContextOptionsBuilder<AlphaTechnologiesRepordCardDbContext>();
+            builder.UseSqlite(_connection);
+
+            return builder.Options;
+        }
+    }
+}
